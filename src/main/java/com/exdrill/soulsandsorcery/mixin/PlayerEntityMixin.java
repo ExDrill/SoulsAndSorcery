42fc1392be0dcbf1c7930ac4b5a2745168954a2f
ExDrill <72329798+ExDrill@users.noreply.github.com>
2022-04-21 13:37:13 -0500

    Update PlayerEntityMixin.java

--- src/main/java/com/exdrill/soulsandsorcery/mixin/PlayerEntityMixin.java
+++ src/main/java/com/exdrill/soulsandsorcery/mixin/PlayerEntityMixin.java
@@ -3,12 +3,10 @@ package com.exdrill.soulsandsorcery.mixin;
 import com.exdrill.soulsandsorcery.misc.PlayerEntityInterface;
 import com.exdrill.soulsandsorcery.registry.ModItems;
 import net.minecraft.entity.ItemEntity;
-import net.minecraft.entity.data.DataTracker;
-import net.minecraft.entity.data.TrackedData;
-import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.world.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,8 +14,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin implements PlayerEntityInterface {
-    private static final TrackedData<Boolean> IS_SOUL_HARVESTER;
     public int soulsAmount = 0;
+    public boolean canSoulHarvest = false;
     PlayerEntity playerEntity = (PlayerEntity) (Object) this;
 
     // Tick
@@ -44,48 +42,39 @@ public class PlayerEntityMixin implements PlayerEntityInterface {
     }
 
 
-    // Nbt Data
-    @Inject(method = "initDataTracker", at = @At("HEAD"))
-    protected void initDataTracker(CallbackInfo ci) {
-        playerEntity.getDataTracker().startTracking(IS_SOUL_HARVESTER, false);
-    }
 
     @Inject(method = "readCustomDataFromNbt", at = @At("HEAD"))
     public void readCustomDataFromNbt(NbtCompound nbt, CallbackInfo ci) {
         this.soulsAmount = nbt.getInt("SoulsAmount");
-        this.setSoulHarvester(nbt.getBoolean("IsSoulHarvester"));
+        this.canSoulHarvest = nbt.getBoolean("IsSoulHarvester");
 
     }
 
     @Inject(method = "writeCustomDataToNbt", at = @At("HEAD"))
     public void writeCustomDataToNbt(NbtCompound nbt, CallbackInfo ci) {
         nbt.putInt("SoulsAmount", this.soulsAmount);
-        nbt.putBoolean("IsSoulHarvester", this.canSoulHarvest());
+        nbt.putBoolean("IsSoulHarvester", this.canSoulHarvest);
     }
 
     @Override
     public boolean canSoulHarvest() {
-        return playerEntity.getDataTracker().get(IS_SOUL_HARVESTER);
+        return this.canSoulHarvest;
     }
 
     @Override
-    public void setSoulHarvester(boolean soulHarvester) {
-        playerEntity.getDataTracker().set(IS_SOUL_HARVESTER, soulHarvester);
+    public void setSoulHarvester(boolean canSoulHarvest) {
+        this.canSoulHarvest = canSoulHarvest;
     }
 
-
-    static {
-        IS_SOUL_HARVESTER = DataTracker.registerData(PlayerEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
-    }
 
     // Petrified Artifact Drop
     @Inject(method = "onDeath", at = @At("HEAD"))
     public void onDeath(CallbackInfo ci) {
-        if (this.canSoulHarvest()) {
+        if (canSoulHarvest && !playerEntity.world.getGameRules().getBoolean(GameRules.KEEP_INVENTORY)) {
             ItemEntity itemEntity = new ItemEntity(playerEntity.world, playerEntity.getX(), playerEntity.getY(), playerEntity.getZ(), new ItemStack(ModItems.PETRIFIED_ARTIFACT));
             playerEntity.world.spawnEntity(itemEntity);
+            System.out.println("Spawned petrified artifact");
         }
-
     }
 }
 
