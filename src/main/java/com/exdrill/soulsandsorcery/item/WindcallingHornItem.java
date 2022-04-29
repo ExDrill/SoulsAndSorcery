20d0ca63e2e70280d153578ecf810c1fdabdb62d
ExDrill <72329798+ExDrill@users.noreply.github.com>
2022-04-28 21:51:48 -0500

    Windcalling Horn finished!

--- src/main/java/com/exdrill/soulsandsorcery/item/WindcallingHornItem.java
+++ src/main/java/com/exdrill/soulsandsorcery/item/WindcallingHornItem.java
@@ -1,11 +1,13 @@
 package com.exdrill.soulsandsorcery.item;
 
 import com.exdrill.soulsandsorcery.access.SoulComponents;
+import com.exdrill.soulsandsorcery.registry.ModSounds;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.MessageType;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.TranslatableText;
@@ -13,10 +15,12 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 public class WindcallingHornItem extends AbstractArtifactItem {
@@ -31,14 +35,25 @@ public class WindcallingHornItem extends AbstractArtifactItem {
         if (((SoulComponents) user).getSouls() >= soulUsage) {
             BlockPos pos = user.getBlockPos();
             ((SoulComponents) user).addSouls(-soulUsage);
-            world.playSound(user, pos, SoundEvents.EVENT_RAID_HORN, SoundCategory.PLAYERS, 50.0F, 1.0F);
+            world.playSound(user, pos, ModSounds.ITEM_WINDCALLING_HORN_BLOW_EVENT, SoundCategory.PLAYERS, 50.0F, 1.0F);
+            // This should affect the entity that is using it
+
             List<? extends LivingEntity> list = world.getEntitiesByClass(LivingEntity.class, new Box(pos).expand(4.0D), (e) -> true);
             LivingEntity livingEntity;
             list.iterator();
             for (Iterator<? extends LivingEntity> var2 = list.iterator(); var2.hasNext(); this.knockback(livingEntity, user)) {
                 livingEntity = var2.next();
             }
-            user.getItemCooldownManager().set(this, 100);
+            Vec3d vec3d = user.getBoundingBox().getCenter();
+            Random random = user.getRandom();
+            for(int i = 0; i < 40; ++i) {
+                double d = random.nextGaussian() * 0.2D;
+                double e = random.nextGaussian() * 0.2D;
+                double f = random.nextGaussian() * 0.2D;
+                world.addParticle(ParticleTypes.POOF, vec3d.x, vec3d.y, vec3d.z, d, e, f);
+            }
+
+            user.getItemCooldownManager().set(this, 200);
             return TypedActionResult.success(user.getStackInHand(hand));
         } else {
             if (world.isClient) {
--- src/main/java/com/exdrill/soulsandsorcery/registry/ModSounds.java
+++ src/main/java/com/exdrill/soulsandsorcery/registry/ModSounds.java
@@ -13,10 +13,12 @@ public class ModSounds {
     public static final Identifier ITEM_PETRIFIED_ARTIFACT_ESCAPE = new Identifier(SoulsAndSorcery.MODID, "item.petrified_artifact.escape");
     public static final Identifier BLOCK_PETRIFIED_GLASS_BREAK = new Identifier(SoulsAndSorcery.MODID, "block.petrified_glass.break");
     public static final Identifier BLOCK_PETRIFIED_GLASS_PLACE = new Identifier(SoulsAndSorcery.MODID, "block.petrified_glass.place");
+    public static final Identifier ITEM_WINDCALLING_HORN_BLOW = new Identifier(SoulsAndSorcery.MODID, "item.windcalling_horn.blow");
     public static SoundEvent ITEM_PETRIFIED_ARTIFACT_ABSORB_EVENT = new SoundEvent(ITEM_PETRIFIED_ARTIFACT_ABSORB);
     public static SoundEvent ITEM_PETRIFIED_ARTIFACT_ESCAPE_EVENT = new SoundEvent(ITEM_PETRIFIED_ARTIFACT_ESCAPE);
     public static SoundEvent BLOCK_PETRIFIED_GLASS_BREAK_EVENT = new SoundEvent(BLOCK_PETRIFIED_GLASS_BREAK);
     public static SoundEvent BLOCK_PETRIFIED_GLASS_PLACE_EVENT = new SoundEvent(BLOCK_PETRIFIED_GLASS_PLACE);
+    public static SoundEvent ITEM_WINDCALLING_HORN_BLOW_EVENT = new SoundEvent(ITEM_WINDCALLING_HORN_BLOW);
 
     public static final BlockSoundGroup PETRIFIED_GLASS  = new BlockSoundGroup(1.0F, 1.0F, BLOCK_PETRIFIED_GLASS_BREAK_EVENT, SoundEvents.BLOCK_GLASS_STEP, BLOCK_PETRIFIED_GLASS_PLACE_EVENT, SoundEvents.BLOCK_GLASS_HIT, SoundEvents.BLOCK_GLASS_FALL);
 
