6061089e6746d6820afe7a69f001ffcebf67bb50
ExDrill <72329798+ExDrill@users.noreply.github.com>
2022-07-08 21:53:35 -0500

    Update PlayerEntityMixin.java

--- src/main/java/com/exdrill/soulsandsorcery/mixin/PlayerEntityMixin.java
+++ src/main/java/com/exdrill/soulsandsorcery/mixin/PlayerEntityMixin.java
@@ -3,18 +3,24 @@ package com.exdrill.soulsandsorcery.mixin;
 import com.exdrill.soulsandsorcery.SoulsAndSorcery;
 import com.exdrill.soulsandsorcery.access.SoulComponents;
 import io.netty.util.SuppressForbidden;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.Arm;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,14 +29,26 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import java.util.Objects;
+import java.util.UUID;
+
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin extends LivingEntity implements SoulComponents {
+
+    @Shadow @Final private PlayerInventory inventory;
+    private static final UUID SOUL_SIPHON_UUID = UUID.fromString("8311f5e9-8e53-4b8d-b7b1-a8bc81fa4af0");
+    private static final TrackedData<Integer> SOULS_AMOUNT = DataTracker.registerData(PlayerEntity.class, TrackedDataHandlerRegistry.INTEGER);
+    private boolean hasChanged;
+
+    PlayerEntity player = (PlayerEntity) (Object) this;
 
     protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
     }
 
-    private static final TrackedData<Integer> SOULS_AMOUNT = DataTracker.registerData(PlayerEntity.class, TrackedDataHandlerRegistry.INTEGER);
+
+
+
 
     // Data Tracker
     @Inject(require = 1, method = "initDataTracker", at = @At("HEAD"))
@@ -59,6 +77,47 @@ public class PlayerEntityMixin extends LivingEntity implements SoulComponents {
         int i = Math.min(soulCount, 20);
         this.dataTracker.set(SOULS_AMOUNT, i);
     }
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void tick(CallbackInfo ci) {
+        if (shouldRemoveSoulSiphon() && this.inventory.getSwappableHotbarSlot() > 0) {
+            removeSoulSiphon();
+        }
+        addSoulSiphon();
+
+    }
+
+    private boolean shouldRemoveSoulSiphon() {
+        ItemStack itemStack = player.getStackInHand(Hand.MAIN_HAND);
+        int i = EnchantmentHelper.getLevel(SoulsAndSorcery.SOUL_SIPHON, itemStack);
+
+        return i == 0;
+    }
+
+    private void addSoulSiphon() {
+        ItemStack itemStack = player.getStackInHand(Hand.MAIN_HAND);
+        int i = EnchantmentHelper.getLevel(SoulsAndSorcery.SOUL_SIPHON, itemStack);
+
+        if (i > 0) {
+            EntityAttributeInstance entityAttributeInstance = this.getAttributeInstance(SoulsAndSorcery.GENERIC_SOUL_GATHERING);
+            if (entityAttributeInstance == null) {
+                return;
+            }
+
+            entityAttributeInstance.addPersistentModifier(new EntityAttributeModifier(SOUL_SIPHON_UUID,"Soul siphon", i, EntityAttributeModifier.Operation.ADDITION));
+
+        }
+    }
+
+    private void removeSoulSiphon() {
+        EntityAttributeInstance entityAttributeInstance = this.getAttributeInstance(SoulsAndSorcery.GENERIC_SOUL_GATHERING);
+        if (entityAttributeInstance != null) {
+            if (entityAttributeInstance.getModifier(SOUL_SIPHON_UUID) != null) {
+                entityAttributeInstance.removeModifier(SOUL_SIPHON_UUID);
+            }
+        }
+    }
+
 
     // Nbt Data
     @Inject(method = "writeCustomDataToNbt", at = @At("HEAD"))
